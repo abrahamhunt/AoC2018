ac9e4c8540e7b020b94555bcb9431feb71c5064d
Abraham Hunt <abrahamhunt@gmail.com>
2018-12-01 06:45:06 -0700

    Initial Commit

---
+++ AdventOfCode2018/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdventOfCode2018
+//
+//  Created by Abraham Hunt on 12/1/18.
+//  Copyright © 2018 Abraham Hunt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
